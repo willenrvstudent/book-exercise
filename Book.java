9593dbb27cded3e27c102322a1636bc214851b4a
Willen Oliveira Leal <43154735+willenrvstudent@users.noreply.github.com>
2018-09-17 20:55:18 -0400

    2.85

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
     
     public void printAuthor()
@@ -30,6 +32,11 @@ class Book
     public void printTitle()
     {
         System.out.print(title);
+    }
+    
+    public int getPages()
+    {
+        return pages;
     }
 
     // Add the methods here ...
